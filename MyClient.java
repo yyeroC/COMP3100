63300b59f4d0abbc3362e11388268189db9b0836
yyeroC <coreylip@gmail.com>
2023-03-30 16:40:37 +1100

    Server sorting and  job scheduling

--- MyClient.java
+++ MyClient.java
@@ -25,13 +25,47 @@
     
     dout.write(("REDY\n").getBytes());
     dout.flush();
+    str = (String)in.readLine();
+    System.out.println(str);
     
-    dout.write(("QUIT\n").getBytes());
+    String[] jobs = str.split(" ", 10);
+    
+    dout.write(("GETS All\n").getBytes());
     dout.flush();
     
     str = (String)in.readLine();
     System.out.println(str);
     
+    dout.write(("OK\n").getBytes());
+    dout.flush();
+    
+    int nServers = Character.getNumericValue(str.charAt(5));
+    int j = 0;
+    String[] largest = new String[10];
+    
+    for(int i = 0; i < nServers; i++) {
+    	str = (String)in.readLine();
+    	System.out.println(str);
+    	
+    	String[] ser = str.split(" ");
+    	int cores = Integer.parseInt(String.valueOf(ser[4]));
+    	
+    	if(j < cores) {
+    		j = cores;
+    		largest = ser;
+    	}
+    }
+    dout.write(("OK\n").getBytes());
+    dout.flush();
+    
+    dout.write(("SCHD " + jobs[2] + " " + largest[0] + " " + largest[1] + "\n").getBytes());
+    dout.flush();
+    
+    dout.write(("OK\n").getBytes());
+    dout.flush();
+    
+    dout.write(("QUIT\n").getBytes());
+    dout.flush();
     s.close();  
     }catch(Exception e){System.out.println(e);}  
     }  
