5387b1afdd452e4fa41756d716225e7ead20fe39
yyeroC <coreylip@gmail.com>
2023-04-03 07:12:28 +1000

    Working method to find largest server type -- Exit while loop condition isn't reached

--- MyClient.java
+++ MyClient.java
@@ -1,6 +1,6 @@
     import java.io.*;  
     import java.net.*;  
-    public class Test {  
+    public class MyClient {  
     
     
     
@@ -10,6 +10,12 @@
     Socket s=new Socket("localhost",50000);  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
     BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));  
+   
+    int cores = 0;
+    String[] largest = new String[10];
+    int count = 0;
+    int schedCount = 0;
+    String name = "";
     
     dout.write(("HELO\n").getBytes());  
     dout.flush();  
@@ -23,13 +29,21 @@
     str = (String)in.readLine();
     System.out.println(str);
     
+    boolean found = false;
+    boolean none = false; 
+    
+   while(none != true) {
     dout.write(("REDY\n").getBytes());
     dout.flush();
     str = (String)in.readLine();
-    System.out.println(str);
     
     String[] jobs = str.split(" ", 10);
+    System.out.println(jobs[0]);
+    if(jobs[0].equals("NONE")) {
+    	none = true;
+    }
     
+    if(found == false) {
     dout.write(("GETS All\n").getBytes());
     dout.flush();
     
@@ -41,26 +55,42 @@
     
     String[] DATA = str.split(" ");
     int nServers = Integer.valueOf(DATA[1]);
-    int j = 0;
-    String[] largest = new String[10];
-    int count = 0;
     for(int i = 0; i < nServers; i++) {
     	str = (String)in.readLine();
     	System.out.println(str);
     	String[] ser = str.split(" ");
-    	int cores = Integer.parseInt(String.valueOf(ser[4]));
     	count++;
-    	if(j < cores) {
-    		j = cores;
+    	int tempCores = Integer.parseInt(String.valueOf(ser[4]));
+    	if(cores < tempCores) {
+    		cores = tempCores;
     		largest = ser;
+    		name = largest[0];
+    		count = 1;
+    	}
+    	if(tempCores == cores && name.equals(ser[0]) == false) {
+    		count--;
     	}
     }
-    dout.write(("OK\n").getBytes());
-    dout.flush();
-    
-    dout.write(("SCHD " + jobs[2] + " " + largest[0] + " " + largest[1] + "\n").getBytes());
-    dout.flush();
-    
+    	found = true;
+    }
+  
+    	dout.write(("OK\n").getBytes());
+   	dout.flush();
+   	 
+   	str = (String)in.readLine();
+    	System.out.println(str);
+    	if(!str.equals(".") || !str.equals("ERR")){
+    	if(jobs[0].equals("JOBN")) {
+    		if(schedCount == count) {
+    			schedCount = 0;
+    		}
+        	dout.write(("SCHD " + jobs[2] + " " + largest[0] + " " + schedCount + "\n").getBytes());
+        	dout.flush();
+        	schedCount++;
+        }
+        }
+   }
+   
     dout.write(("OK\n").getBytes());
     dout.flush();
     
